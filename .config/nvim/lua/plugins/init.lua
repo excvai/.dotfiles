30a1d5b9aa3c3f7de76ea0d099983d00a7ea38da
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2024-04-17 00:18:57 +0300

    Improve indent identification

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,5 +4,5 @@ return {
   "antoinemadec/FixCursorHold.nvim", -- this is needed to fix lsp doc highlight
   "tpope/vim-surround",
   "psliwka/vim-smoothie",
-  "Darazaki/indent-o-matic",
+  "tpope/vim-sleuth" -- heuristically set buffer options (mostly for indenting)
 }
