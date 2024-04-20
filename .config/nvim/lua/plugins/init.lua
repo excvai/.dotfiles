4191f541155fefd31c051449b7c2391c71bec26c
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2024-04-21 01:10:34 +0300

    Add vim-fugitive

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,5 +4,6 @@ return {
   "antoinemadec/FixCursorHold.nvim", -- this is needed to fix lsp doc highlight
   "tpope/vim-surround",
   "psliwka/vim-smoothie",
-  "tpope/vim-sleuth" -- heuristically set buffer options (mostly for indenting)
+  "tpope/vim-sleuth", -- heuristically set buffer options (mostly for indenting)
+  "tpope/vim-fugitive"
 }
