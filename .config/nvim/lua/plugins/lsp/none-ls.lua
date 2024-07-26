4bfd68ee3a29112ccf16a6944b750e4bc1e222ce
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2024-07-26 19:34:23 +0300

    Update none-ls

--- .config/nvim/lua/plugins/lsp/none-ls.lua
+++ .config/nvim/lua/plugins/lsp/none-ls.lua
@@ -2,6 +2,7 @@ return {
   "nvimtools/none-ls.nvim", -- configure formatters & linters
   dependencies = {
     "jay-babu/mason-null-ls.nvim",
+    "nvimtools/none-ls-extras.nvim"
   },
   config = function()
     local null_ls = require("null-ls")
@@ -9,14 +10,11 @@ return {
     require("mason-null-ls").setup({
       ensure_installed = {
         -- "prettierd", -- prettier formatter
+        -- "prettier" -- installed locally
         "stylua", -- lua formatter
         "eslint_d", -- js linter
       },
     })
-
-    local formatting = null_ls.builtins.formatting -- to setup formatters
-    local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-    local code_actions = null_ls.builtins.code_actions
 
     -- -- to setup format on save
     -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -26,17 +24,17 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
       -- setup formatters & linters
       sources = {
-        formatting.prettier.with({
+        null_ls.builtins.formatting.prettier.with({
           extra_filetypes = { "svelte" },
           -- disabled_filetypes = { "html" },
         }), -- js/ts formatter
-        formatting.stylua, -- lua formatter
-        diagnostics.eslint_d.with({ -- js/ts linter
+        null_ls.builtins.formatting.stylua, -- lua formatter
+        require('none-ls.diagnostics.eslint_d').with({ -- js/ts linter
           condition = function(utils)
             return utils.root_has_file({ ".eslintrc", ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" }) -- only enable if root has one of specified files
           end,
         }),
-        code_actions.eslint_d,
+        require('none-ls.code_actions.eslint_d'),
       },
       -- -- configure format on save
       -- on_attach = function(current_client, bufnr)
