d1cd0f8dbc8f6d0bb19e5e06c1452897adaf690e
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2024-07-31 00:02:14 +0300

    Replace vim-surround with nvim-surround

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,9 +2,8 @@
 return {
   "moll/vim-bbye",
   "antoinemadec/FixCursorHold.nvim", -- this is needed to fix lsp doc highlight
-  "tpope/vim-surround",
   "psliwka/vim-smoothie",
   "tpope/vim-sleuth", -- heuristically set buffer options (mostly for indenting)
   "tpope/vim-fugitive",
-  "mbbill/undotree"
+  "mbbill/undotree",
 }
